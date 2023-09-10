faae24ec7742b3e25debc681e118979b284599d5
valmirfd <fernandocursos2021@gmail.com>
2023-09-09 23:48:49 -0300

    Criação das telas de login, cadastro e reset

---
+++ app/src/main/java/com/valorizeseusamigos/fala_comigo/autenticacao/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.valorizeseusamigos.fala_comigo.autenticacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.valorizeseusamigos.fala_comigo.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
